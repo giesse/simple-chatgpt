018261dbb2412d76383015a0e179f9b064ece5db
Gabriele Santilli <gabriele@colellachiara.com>
2023-04-24 14:20:47 +0200

    Now saves out conversations

--- chatgpt.py
+++ chatgpt.py
@@ -1,26 +1,46 @@
 import openai
 import toml
 import argparse
-#import sys
+import copy
+import os
+import yaml
 
 # Read API key from config.toml
 config = toml.load("config.toml")
 openai.api_key = config["openai"]["api_key"]
 
-def generate_text(prompt, messages, model='gpt-3.5-turbo'):
-    try:
-        if not messages:
-            messages.append({"role": "system", "content": "You are a helpful assistant."})
+def init_conversation(model='gpt-3.5-turbo'):
+    return {
+            "model": model,
+            "messages": [{"role": "system", "content": config["openai"]["system_message"]}],
+            "file": None,
+            "title": None,
+            "total_tokens": 0
+            }
 
-        messages.append({"role": "user", "content": prompt})
+def save_conversation(conversation):
+    if not conversation["file"]:
+        conversation["title"] = input("Title: ")
+        conversation["file"] = conversation["title"].replace(" ", "-") + ".yaml"
+
+    file_path = os.path.join("conversations", conversation["file"])
+    print(f"Saving conversation to {file_path}...")
+    yaml_string = yaml.dump(conversation)
+    with open(file_path, "w") as f:
+        f.write(yaml_string)
+
+def generate_text(conversation, prompt):
+    try:
+        conversation["messages"].append({"role": "user", "content": prompt})
 
         response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages
+            model=conversation["model"],
+            messages=conversation["messages"]
         )
 
         generated_text = response.choices[0].message["content"].strip()
-        messages.append({"role": "assistant", "content": generated_text})
+        conversation["messages"].append({"role": "assistant", "content": generated_text})
+        conversation["total_tokens"] += response.usage.total_tokens
         return generated_text
     except Exception as e:
         print(f"Error generating text: {e}")
@@ -33,19 +53,20 @@ def main():
     args = parser.parse_args()
 
     # Initialize an empty conversation
-    messages = []
+    conversation = init_conversation(args.model)
 
     while True:
         # Get user input
-        prompt = input("User: ")
+        prompt = input(">>> ")
 
         # Exit the loop if the user enters 'quit'
         if prompt.lower() == "quit":
+            save_conversation(conversation)
             break
 
         # Generate text
-        generated_output = generate_text(prompt, messages, model=args.model)
-        print(f"Assistant: {generated_output}")
+        generated_output = generate_text(conversation, prompt)
+        print(generated_output)
 
 if __name__ == "__main__":
     main()
