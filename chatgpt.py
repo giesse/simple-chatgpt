0003060e52076e41888810da7cdaa052aff81cb6
Gabriele Santilli <gabriele@colellachiara.com>
2023-04-24 13:10:35 +0200

    Basic ChatGPT

--- chatgpt.py
+++ chatgpt.py
@@ -1,23 +1,27 @@
 import openai
 import toml
 import argparse
-import sys
+#import sys
 
 # Read API key from config.toml
 config = toml.load("config.toml")
 openai.api_key = config["openai"]["api_key"]
 
-def generate_text(prompt, model='gpt-3.5-turbo'):
+def generate_text(prompt, messages, model='gpt-3.5-turbo'):
     try:
+        if not messages:
+            messages.append({"role": "system", "content": "You are a helpful assistant."})
+
+        messages.append({"role": "user", "content": prompt})
+
         response = openai.ChatCompletion.create(
             model=model,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt}
-            ]
+            messages=messages
         )
 
-        return response.choices[0].message["content"].strip()
+        generated_text = response.choices[0].message["content"].strip()
+        messages.append({"role": "assistant", "content": generated_text})
+        return generated_text
     except Exception as e:
         print(f"Error generating text: {e}")
         return None
@@ -28,12 +32,20 @@ def main():
     parser.add_argument("-m", "--model", default="gpt-3.5-turbo", help="Model to use (default: gpt-3.5-turbo)")
     args = parser.parse_args()
 
-    # Get user input
-    prompt = input("Enter your prompt: ")
+    # Initialize an empty conversation
+    messages = []
 
-    # Generate text
-    generated_output = generate_text(prompt, model=args.model)
-    print(f"Generated text: {generated_output}")
+    while True:
+        # Get user input
+        prompt = input("User: ")
+
+        # Exit the loop if the user enters 'quit'
+        if prompt.lower() == "quit":
+            break
+
+        # Generate text
+        generated_output = generate_text(prompt, messages, model=args.model)
+        print(f"Assistant: {generated_output}")
 
 if __name__ == "__main__":
     main()
