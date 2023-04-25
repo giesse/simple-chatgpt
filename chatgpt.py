f7e082731ddb1ab445aff2ab5e26eda618bf2847
Gabriele Santilli <gabriele@colellachiara.com>
2023-04-25 21:43:27 +0200

    Refactored code

--- chatgpt.py
+++ chatgpt.py
@@ -1,88 +1,81 @@
 import openai
 import toml
 import argparse
-import copy
 import os
 import yaml
 
-# Load the OpenAI API key from the config file
 config = toml.load("config.toml")
 openai.api_key = config["openai"]["api_key"]
 
-# Initialize a conversation and return it
-def init_conversation(model='gpt-3.5-turbo'):
-    return {
-            "model": model,
-            "messages": [{"role": "system", "content": config["openai"]["system_message"]}],
-            "file": None,
-            "title": None,
-            "total_tokens": 0
-            }
+class Conversation:
+    def __init__(self, model='gpt-3.5-turbo', file=None):
+        self.model = model
+        self.file = file
+        self.title = None
+        self.total_tokens = 0
+        self.messages = [{"role": "system", "content": config["openai"]["system_message"]}]
+        
+        if self.file:
+            self.load()
 
-# Save the conversation to a file
-def save_conversation(conversation):
-    # If the file name isn't set, ask for it and set it
-    if not conversation["file"]:
-        conversation["title"] = input("Title: ")
-        conversation["file"] = conversation["title"].replace(" ", "-") + ".yaml"
+    def save(self):
+        # Set the file name based on the title, if not already set
+        if not self.file:
+            self.title = input("Title: ")
+            self.file = self.title.replace(" ", "-") + ".yaml"
 
-    # Write the conversation to a file
-    file_path = os.path.join("conversations", conversation["file"])
-    print(f"Saving conversation to {file_path}...")
-    yaml_string = yaml.dump(conversation)
-    with open(file_path, "w") as f:
-        f.write(yaml_string)
+        # Save the conversation data to a YAML file in the "conversations" directory
+        file_path = os.path.join("conversations", self.file)
+        print(f"Saving conversation to {file_path}...")
+        yaml_string = yaml.dump(self.__dict__)
+        with open(file_path, "w") as f:
+            f.write(yaml_string)
 
-# Load a conversation from a file
-def load_conversation(file):
-    with open(file, "r") as f:
-        conversation = yaml.load(f, Loader=yaml.FullLoader)
+    def load(self):
+        # Load conversation data from the provided YAML file
+        with open(self.file, "r") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
 
-    return conversation
+        # Update conversation attributes with the loaded data
+        self.__dict__.update(data)
 
-# Print the conversation
-def print_conversation(conversation):
-    for message in conversation["messages"]:
-        text = message["content"]
-        role = message["role"]
-        if role == "system":
-            print(f"[{text}]")
-        elif role == "user":
-            print(f">>> {text}")
-        elif role == "assistant":
-            print(f"{text}\n\n")
+    def print(self):
+        print(f"Model: {self.model}\n")
+        for message in self.messages:
+            text = message["content"]
+            role = message["role"]
+            if role == "system":
+                print(f"[{text}]")
+            elif role == "user":
+                print(f">>> {text}")
+            elif role == "assistant":
+                print(f"{text}\n")
 
-# Generate text using OpenAI's API
-def generate_text(conversation, prompt, role="user"):
-    try:
-        # Add the prompt to the conversation messages
-        conversation["messages"].append({"role": role, "content": prompt})
+    def generate_text(self, prompt, role="user"):
+        try:
+            # Add the user prompt to the conversation messages
+            self.messages.append({"role": role, "content": prompt})
 
-        # Make an API call to OpenAI's chat model
-        response = openai.ChatCompletion.create(
-            model=conversation["model"],
-            messages=conversation["messages"]
-        )
+            # Make an API call to OpenAI's chat model
+            response = openai.ChatCompletion.create(
+                model=self.model,
+                messages=self.messages
+            )
 
-        # Get the generated text from the API response
-        generated_text = response.choices[0].message["content"].strip()
+            # Extract the generated text from the API response
+            generated_text = response.choices[0].message["content"].strip()
+            # Add the generated text to the conversation messages
+            self.messages.append({"role": "assistant", "content": generated_text})
+            # Update the total tokens used in the conversation
+            self.total_tokens += response.usage.total_tokens
 
-        # Add the generated text to the conversation messages
-        conversation["messages"].append({"role": "assistant", "content": generated_text})
+            return generated_text
+        except Exception as e:
+            print(f"Error generating text: {e}")
+            return None
 
-        # Record the number of tokens used for the API call
-        conversation["total_tokens"] += response.usage.total_tokens
-
-        return generated_text
-    except Exception as e:
-        # Handle errors that occur while generating text
-        print(f"Error generating text: {e}")
-        return None
-
-# Get user input (either multiline or single line)
 def get_input(multiline):
     if multiline:
-        # Let the user input multiple lines (ended by a line with '.')
         result = input(">>> [end with .]\n")
         while True:
             line = input()
@@ -91,40 +84,37 @@ def get_input(multiline):
             result += "\n" + line
         return result
     else:
-        # Let the user input a single line
         return input(">>> ")
 
-# Main function to run the conversation
 def main():
-    # Command line argument parser
     parser = argparse.ArgumentParser(description="Simple ChatGPT")
     parser.add_argument("-c", "--conversation", help="Conversation file (to resume a previous conversation)")
     parser.add_argument("-m", "--model", default="gpt-3.5-turbo", help="Model to use (default: gpt-3.5-turbo)")
     args = parser.parse_args()
 
-    # Load the conversation from a file if given, otherwise initialize a new one
+    # Initialize a Conversation object, either by loading an existing conversation from a file or creating a new one
     if args.conversation:
-        conversation = load_conversation(args.conversation)
+        conversation = Conversation(file=args.conversation)
     else:
-        conversation = init_conversation(args.model)
+        conversation = Conversation(model=args.model)
 
     # Print the conversation so far
-    print_conversation(conversation)
+    conversation.print()
 
     while True:
-        # Get user input
+        # Get user input based on the "multiline" setting in the config file
         prompt = get_input(config["openai"]["multiline"])
 
-        # Exit the loop if the user enters 'quit'
+        # Exit the loop and save the conversation if the user enters 'quit'
         if prompt.lower() == "quit":
-            save_conversation(conversation)
+            conversation.save()
             break
 
         # Generate text using OpenAI's API
-        generated_output = generate_text(conversation, prompt)
+        generated_output = conversation.generate_text(prompt)
 
         # Display the generated text
-        print(f"{generated_output}\n\n")
+        print(f"{generated_output}\n")
 
 if __name__ == "__main__":
     main()
